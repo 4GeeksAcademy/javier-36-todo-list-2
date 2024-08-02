1127ed98b426cf16af0699d902bea5000653d9d5
javierfebres36 <164325528+javierfebres36@users.noreply.github.com>
2024-08-02 14:54:34 +0000

    first-commit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,4 +1,6 @@
+import { useEffect } from "react";
 import React, { useState } from "react";
+
 
 
 
@@ -10,42 +12,116 @@ const Home = () => {
 	const [todo, setTodo] = useState([]);
 	const [inputValue, setInputValue] = useState("")
 
+	const obtenerTareas = () => {
+		fetch("https://playground.4geeks.com/todo/users/javier36")
+			.then(resp => {
+				console.log(resp);
+				if (!resp.ok) {
+					crearAgendas()
+					
+
+				} else {
+					return resp.json()
+				}
+			})
+			.then(data => setTodo(data.todos))
+			.catch(error => console.log(error))
+
+	}
+	const crearAgendas = () => {
+		fetch("https://playground.4geeks.com/todo/users/javier36", {
+			method: "POST",
+			headers:
+			{
+				"Content-Type": "application/json",
+				
+			},
+		})
+			.then((response) => response.json())
+			.then((result) => obtenerTareas())
+			.catch((error) => console.error(error));
+	};
+
+	const crearTareas = () => {
+		fetch("https://playground.4geeks.com/todo/todos/javier36", {
+			method: "POST",
+			headers:
+			{
+				"Content-Type": "application/json",
+				
+			},
+			body:JSON.stringify ({
+				label:inputValue,
+				is_done:false
+			})
+		})
+			.then((response) => response.json())
+			.then((result) => {
+				obtenerTareas()
+			setInputValue("")
+			})
+			.catch((error) => console.error(error));
+
+	}
+
+
+
+
+
+
+	useEffect(() => {
+		obtenerTareas()
+	}, [])
+
+
+
 	return (
-		<div className="card container">
+
+		<div className="card mb-3">
 			<h1>my todo </h1>
-			<ul>
-				<li>
-					<input
-						type="text"
-						onChange={(e) => setInputValue(e.target.value)}
-						value={inputValue}
-						onKeyPress={(e) => {
-							if (e.key === "Enter") {
-								setTodo(todo.concat([inputValue]));
-								setInputValue("");
-							}
-						}}
-						placeholder="firs-name"></input>
-
-				</li>
-
-				{todo.map((item, index) => (
+			<div className="row g-0 ">
+				<ul>
 					<li>
-						{item} {""}
-						<i class="fa-solid fa-trash"
-							onClick={() =>
-								setTodo(
-									todo.filter(
-										(t, currentIndex) =>
-											index != currentIndex
-									)
-								)
-							}></i>
-					</li>
-				))}
+						<input
+							type="text"
+							onChange={(e) => setInputValue(e.target.value)}
+							value={inputValue}
+							onKeyDown={(e) => {
+								if (e.key === "Enter") {
+									crearTareas ()
+									
+								}
+							}}
+							placeholder="firs-name"></input>
 
-			</ul>
-			<div>{todo.length}text</div>
+					</li>
+
+					{todo.map((item, index) => (
+						<li>
+							{item.label} {""}
+							<i class="fa-solid fa-trash"
+								onClick={() =>
+									setTodo(
+										todo.filter(
+											(t, currentIndex) =>
+												index != currentIndex
+										)
+									)
+								}></i>
+						</li>
+					))}
+
+				</ul>
+
+			</div>
+
+
+			<div>{todo.length}text
+
+
+			</div>
+
+
 
 		</div>
 	);
